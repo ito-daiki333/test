cf0feb84c894ce9f01d1c3a5a4edf7d697d0a821
ito-daiki333 <adoresukaetemitad@ezweb.ne.jp>
2019-04-01 23:31:55 +0900

    仕事が辛い

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print ("Hello World")
+print ("Hello World\n")
+
+print ("あなたは仕事が辛いですか？\n")
+print ("はい：いいえ")
+
+death = input()
+
+if (death == "はい") :
+    print ("明日は有給取って休んでください")
+
+elif (death == "いいえ") :
+    print ("そんな事を言っていられるのも今のうちだ")
+
+else :
+    print ("はい　か　いいえで答えろよくそ")
